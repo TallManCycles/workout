51303e6833de0b645ea555a693c6bc09d9d6964f
TallManCycles <Aaronday@live.com.au>
2024-05-06 18:58:47 +1000

    updates

--- src/components/WorkoutsComponent.vue
+++ src/components/WorkoutsComponent.vue
@@ -1,3 +1,32 @@
 <template>
     <h1 class="text-center">Start A Workout</h1>
+    <h2 class="pt-3">Select a workout</h2>
+    <v-select
+        :items="workouts"
+        label="Workout"
+        v-model="selectedWorkout"
+    ></v-select>
+    <v-btn
+    color="primary"
+    large block
+    class="mt-5">
+    Start
+  </v-btn>
+  <v-btn
+    color="success"
+    large block
+    class="mt-5">
+    Create New Workout
+  </v-btn>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            selectedWorkout: null,
+            workouts: ['Workout 1', 'Workout 2', 'Workout 3']
+        }
+    }
+}
+</script>
