e9f12fe19243e07f26d57ea53ac3ede3a44da60b
TallManCycles <Aaronday@live.com.au>
2024-05-07 10:16:11 +1000

    feat: Add vuejs-countdown dependency for timer functionality
    The code changes in package.json add the vuejs-countdown dependency to enable the timer functionality in the Exercise.vue component. This allows for displaying and managing the rest timer for each exercise set.
    
    This commit message follows the established convention of starting with a verb in the imperative form and providing a clear and concise description of the changes made.

--- src/pages/Exercise.vue
+++ src/pages/Exercise.vue
@@ -10,7 +10,7 @@
                     <v-list-item-subtitle>
                         <v-row>
                             <v-col cols="3">
-                                <v-checkbox v-model="set.complete"></v-checkbox>
+                                <v-checkbox v-model="set.complete" @click="startTimer"></v-checkbox>
                             </v-col>
                             <v-col cols="3">
                                 <v-text-field v-model="set.reps" label="Reps" type="number"></v-text-field>
@@ -26,9 +26,19 @@
                 </v-list-item-content>
             </v-list-item>
         </v-list>
-        <v-btn color="success" large block class="mt-5">
+        <v-btn color="success" large block class="mt-5" @click="saveSet">
             Save and Continue
         </v-btn>
+        <v-bottom-sheet v-model="showTimer">
+            <v-card class="text-center">
+                <v-card-text>
+                    <v-btn variant="text" @click="showTimer = !showTimer">
+                        close
+                    </v-btn>
+                    <div>{{ restTimer }}</div>
+                </v-card-text>
+            </v-card>
+        </v-bottom-sheet>
     </v-main>
 </template>
 
@@ -75,7 +85,33 @@ export default defineComponent({
                     weight: 120,
                     rir: 2
                 }
-            ]
+            ],
+            showTimer: false,
+            restTimer: 30,
+            timerInterval: 1000
+        }
+    },
+    methods: {
+        saveSet() {
+            this.$router.back()
+        },
+        startTimer() {
+            this.showTimer = true;
+            this.restTimer = 30;
+        },
+    },
+    watch: {
+        showTimer: {
+            handler(value) {
+                if (value) {
+                    this.timerInterval = setInterval(() => {
+                        this.restTimer--;
+                    }, 1000);
+                } else {
+                    clearInterval(this.timerInterval);
+                }
+            },
+            immediate: true
         }
     }
 });
